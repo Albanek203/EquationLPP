9f89d95c09df89cbaa5b785e49d9af5871d52870
Albanek203 <oleglovchuk@ukr.net>
2022-02-20 13:27:24 +0200

    added a function to identify common points & rounding coordinates to 2 points after the comma & added Boolean variables for orientation

--- EquationLPP/models/Equation.cs
+++ EquationLPP/models/Equation.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows.Media;
 
 namespace EquationLPP.models {
@@ -10,7 +11,9 @@ namespace EquationLPP.models {
         public string Data { get; set; } = "";
         public Brush Color { get; set; }
         public Point[] TwoPintsLines = new Point[2];
-        public string StringPoints { get; set; } = "hello";
+        public bool IsParsed = false;
+        public bool IsFirstQuarterP1 = false;
+        public bool IsFirstQuarterP2 = false;
         public Equation(SolidColorBrush color) { this.Color = color; }
         public bool ParseData() {
             Data = Data.Trim();
@@ -18,22 +21,38 @@ namespace EquationLPP.models {
             var groupX2 = new Regex(@"([+-]?\d*)x2").Match(Data).Groups[1];
             var groupSign = new Regex(@"([<>]?=)").Match(Data).Groups[1];
             var groupEqual = new Regex(@"(?<=[<>]?=)[+-]?\d*").Match(Data);
-            if (!groupX1.Success || !groupX2.Success || !groupSign.Success || !groupEqual.Success) { return false; }
+            if (!groupX1.Success || !groupX2.Success || !groupSign.Success || !groupEqual.Success) {
+                IsParsed = false;
+                return false;
+            }
             int.TryParse(groupX1.ToString(), out var tmp);
             CoefficientX1 = tmp == 0 ? 1 : tmp;
             int.TryParse(groupX2.ToString(), out tmp);
             CoefficientX2 = tmp == 0 ? 1 : tmp;
             int.TryParse(groupEqual.ToString(), out tmp);
             Equal = tmp;
-            TwoPintsLines = new[]
-                { new Point(0, (float)Equal / CoefficientX2), new Point((float)Equal / CoefficientX1, 0) };
+            TwoPintsLines = new[] {
+                new Point(0, (float)Math.Round((float)Equal / CoefficientX2, 2))
+              , new Point((float)Math.Round((float)Equal / CoefficientX1, 2), 0)
+            };
             Sign = groupSign.ToString() switch {
                 ">=" => Signs.GreaterThan
               , "<=" => Signs.LessThan
               , "="  => Signs.Equal
               , _    => Sign
             };
+            IsFirstQuarterP1 = TwoPintsLines[0].X >= 0 && TwoPintsLines[0].Y >= 0;
+            IsFirstQuarterP2 = TwoPintsLines[1].X >= 0 && TwoPintsLines[1].Y >= 0;
+            IsParsed = true;
             return true;
+        }
+        public Point GetCrossing(Equation equation) {
+            var cofX2 = -CoefficientX1 * equation.CoefficientX2 - -equation.CoefficientX1 * CoefficientX2;
+            var constant = equation.CoefficientX2 * Equal - CoefficientX2 * equation.Equal;
+            if (cofX2 == 0) { return null; }
+            var x = (float)Math.Round((float)-constant / cofX2, 2);
+            var y = (Equal - CoefficientX1 * x) / CoefficientX2;
+            return new Point(x, (float)Math.Round(y, 2));
         }
     }
 }
